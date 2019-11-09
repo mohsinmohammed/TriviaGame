83b3b380bb3b00b6529f5b764ac3882f28052779
Mohsin Mohammed <mohsin.mohammed4567@gmail.com>
2019-11-09 16:21:31 -0500

    updated app.js with answers evaluations and displaying of results plus added readme-file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# TriviaGame
+### Trivia Game
+
+## Project Description:
+
+In Trivia Game, a player is shown questions that can be answered by clicking on the multiple choices. A count down clock appears with each question and player need to answer the question in a given time. When player clicks on an option, player's choice is evaluated and informed if correct or incorrect answer was chosen. And if the time runs out before the question is answered, the correct answer is displayed. A few seconds after displaying the correct answer, the next question is displayed without any player's input. When all questions of the game are displayed, the program will show the final results of the game. Player will know the number of correct, incorrect, and unanswered questions.     
+ 
+## Project use:
+The project is deployed in Github pages. People can access the game website and play the game for fun.  
+    Here is the link to the website: https://mohsinmohammed.github.io/TriviaGame/
+
+## To get started:
+Clone the project to your account or personel computer. The index file should be opened in a web browser. 
+The project can be found on Github: https://github.com/mohsinmohammed/TriviaGame.git
+
+## Help with project
+    Contact: mohsin.mohammed4567@gmail.com
+
+## Maintains and contributes to the project
+    Author - Mohsin Mohammed
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,28 +3,37 @@ $(document).ready(function() {
     /// trivia object with questions, choices, and answers
 var trivia = { question: ["In which sport would a pommel horse be used?","To which type of leisure activity does the term 'cruciverbalism' refer?", "What city has been dubbed the Chocolate Capital of the United States?","What planet, known as the Morning or Evening Star, is one of only two that don't have any moons?",  "What was the name of the first manned spacecraft to land on the moon?", "In the saying of eighteenth century literature. What is it specifically 'in time' that 'saves nine'?","Who established Pennsylvania?", "What fruit appears on the first screen in the original 'Pac-Man' arcade game?" ], 
                     choices: [["Fencing", "Horse racing", "Gymnastics", "Horse Polo"],["Creative writing", "Traveling on a ship", "Blogging", "Crosswords"], ["Carmel, CA", "Hershey, PA", "Cocoa, FL", "Ding Dong, TX"],["Venus", "Jupiter", "Saturn", "Uranus"], ["Apollo", "Columbia", "Eagle", "Enterprise"],["A twitch", "A stitch", "A catch", "A good line"], ["Benjamin Franklin", "William Penn", "John Hancock", "Thomas Jefferson"],["Bananas", "Grapes", "Oranges", "Cherry"]], 
-                      answer: ["Gymnastics", "Crosswords", "Hershey, PA","Venus", "A stitch", "Eagle", "William Penn","Cherry"],
-                       userAnswer: [-1, -1, -1, -1 , -1 , -1, -1, -1]
+                      answer: ["Gymnastics", "Crosswords", "Hershey, PA","Venus", "Eagle", "A stitch", "William Penn","Cherry"],
+                       userAnswer: []
     };
 // declaring global variables
-var number, intervalId;
-var numQuestion = 0, correctAnswer = 0, incorrectAnswer = 0, unanswered = 8 - correctAnswer - incorrectAnswer;
+var number, intervalId, message = "";
+var numQuestion = 0, correctAnswer = 0, incorrectAnswer = 0, unanswered = 0;
 
 ///  start button calls the start game function
-
-
 $("#start").on("click", startGame);
 
+//This function reset the variable and allows the user to play multiple times
 function startGame(){
     $("#start").css("display","none");  
+    $(".final-result").css("display","none");
+    numQuestion = 0, correctAnswer = 0, incorrectAnswer = 0, unanswered = 0;
 
+    for(var j=0; j<8; j++)
+      trivia.userAnswer[j] = -1;
+    
+      //Here showQuestion is called which starts the game
     showQuestion();
 }
 
+//In showQuestion function we start the clock and display the questions
 function showQuestion(){
     startClock();
+    //Before displaying any question we change the css property of display-answer class
     $(".display-answer").css("display","none");
+    //then we make the Q-A class that contain the question and multiple choices become visible to the user
     $(".Q-A").css("display","block");
+    // text is added to html tags using the tags
     $(".question").text(trivia.question[numQuestion]);
     $(".ch-a").text(trivia.choices[numQuestion][0]);
     $(".ch-b").text(trivia.choices[numQuestion][1]);
@@ -32,42 +41,41 @@ function showQuestion(){
     $(".ch-d").text(trivia.choices[numQuestion][3]);
 }
 
-
+// StartClock function start a count down clock beginning at 10
+// when clock runs out and the number counter goes to 0, the clock stops, and the correct answer is displayed
 function startClock(){   
     $(".display-time").css("display","block");
       number = 10;
-      clearInterval(intervalId);
+      // The setInterval decreases the number which is initialized to 10 after every 1 second with call to 'decrement' function
       intervalId = setInterval(decrement, 1000);
     function decrement() {
       number--;
       $(".time").text(number);
       if (number === 0) {
-        
+           //As the number goes to 0 the clock is stopped and correct answer is displayed
           stopClock();
           displayAnswer();
       }
     }
 }
+
+// stopClock clears the intervalId variable 
+// this method get calls when user click on any of the multiple choices or counter in the startClock method goes to zero
 function stopClock(){   
     clearInterval(intervalId); 
     $(".time").text("10");
 }
 
-
-
-
 $('.cho').hover(
-				
     function () {
        $(this).css({"background-color":"rgb(215, 228, 150)"});
-    }, 
-     
+    },    
     function () {
        $(this).css({"background-color":"rgb(202, 204, 90)"});
     }
  );
 
-
+// on click calls a specific function and sets the user answer in the object which is then evaluted in evaluateUserChoice function
  $(".ch-a").on("click", userChoiceA);
  $(".ch-b").on("click", userChoiceB);
  $(".ch-c").on("click", userChoiceC);
@@ -75,7 +83,6 @@ $('.cho').hover(
 
 
 function userChoiceA() {
-    questionAnswered = true;
     trivia.userAnswer[numQuestion] = 0;
     stopClock();
     evaluateUserChoice();
@@ -83,7 +90,6 @@ function userChoiceA() {
 }
 
 function userChoiceB() {
-  questionAnswered = true;
   trivia.userAnswer[numQuestion] = 1;
   stopClock();
   evaluateUserChoice();
@@ -91,7 +97,6 @@ function userChoiceB() {
 }
 
 function userChoiceC() {
-  questionAnswered = true;
   trivia.userAnswer[numQuestion] = 2;
   stopClock();
   evaluateUserChoice();
@@ -99,7 +104,6 @@ function userChoiceC() {
 }
 
 function userChoiceD() {
-  questionAnswered = true;
   trivia.userAnswer[numQuestion] = 3;
   stopClock();
   evaluateUserChoice();
@@ -108,40 +112,93 @@ function userChoiceD() {
 
 var userChoice;
 function evaluateUserChoice(){  
+  // here we compare the user choices with the correct answer and if it matches the correctAnswer counter is incremented
+  // if it do not match incorrectAnswer is incremented
    if(trivia.choices[numQuestion][trivia.userAnswer[numQuestion]] === trivia.answer[numQuestion])
-        correctAnswer++;
+   {
+     correctAnswer++;
+     message = "Congratulations!! Correct Answer"
+   }
    else
-      incorrectAnswer++;
+   {
+       incorrectAnswer++;
+       message = "That's Incorrect"
+   }
 }
     
 
 function displayAnswer(){
+    // function shows the the choice selected by user and the correct answer of questions
 
     $(".display-time").css("display","none");
     $(".Q-A").css("display","none");
+    $(".display-answer").empty();
     $(".display-answer").css("display","block");
     if(trivia.userAnswer[numQuestion] == -1)
-    $(".player-answer").text("Out of time");
-    else
-    $(".player-answer").text("Your Answer: " + trivia.choices[numQuestion][trivia.userAnswer[numQuestion]]);
-    $(".correct-answer").text("Correct Answer: " + trivia.answer[numQuestion]);
-    numQuestion++;
+    {
+      $(".display-answer").append("<p> Time's up <\p>");
+      $(".display-answer").append("Correct Answer: " + trivia.answer[numQuestion]);
+    }
+    
 
+    if(trivia.choices[numQuestion][trivia.userAnswer[numQuestion]] === trivia.answer[numQuestion])
+   {
+    $(".display-answer").append("<p>You Answered: " + trivia.choices[numQuestion][trivia.userAnswer[numQuestion]] +"<\p>");
+    $(".display-answer").append("<p>" + message + "<\p>");
+   }
+    else
+    {
+      $(".display-answer").append("<p>You Answered: " + trivia.choices[numQuestion][trivia.userAnswer[numQuestion]] +"<\p>");
+      $(".display-answer").append("<p>" + message + "<\p>");
+      $(".display-answer").append("Correct Answer: " + trivia.answer[numQuestion]);
+    }
+    
+    
+    // to show the next question the variable numQuestion is incremented
+    numQuestion++;
+  //check if the numQuestion variable if it is less than 8 we show the next question after 3 sec 
+  //if numQuestion is equal to 8 final results are displayed in finalResult function
    if(numQuestion < 8)
    {
     setTimeout(showQuestion,3000); 
+    message = "";
     }
     else
     {
-      var unanswered = 8 - correctAnswer - incorrectAnswer; 
-      console.log("Total correct answers: " + correctAnswer);
-      console.log("Total incorrect answers: " + incorrectAnswer);
-      console.log("Total unanswered: " +  unanswered);
       setTimeout(finalResult, 3000); 
+      message = "";
     }
 }
 
 function finalResult(){
+
+  unanswered = 8 - correctAnswer - incorrectAnswer; 
+  $(".display-answer").css("display","none");
+  $(".final-result").css("display","block");
+
+    $(".final-result").empty();
+
+    var corrAnsHolder = document.createElement("p");
+    $(".final-result").append(corrAnsHolder);
+    $(corrAnsHolder).text("Total Correct Answers: " + correctAnswer);
+
+    var inCorrAnsHolder = document.createElement("p");
+    $(".final-result").append(inCorrAnsHolder);
+    $(inCorrAnsHolder).text("Total Incorrect Answers: " + incorrectAnswer);
+
+    var unAnsHolder = document.createElement("p");
+    $(".final-result").append(unAnsHolder);
+    $(unAnsHolder).text("Total Unanswered: " + unanswered);
+
+    // Restart button is created and appended to final result tag 
+    // The game restarts when the button is clicked 
+    var restartBttn = document.createElement("button");
+    $(".final-result").append(restartBttn);
+    $(restartBttn).css("background-color", "rgb(196, 223, 171");
+    $(restartBttn).css("font-size", "25px");
+    $(restartBttn).text(" Restart ");
+
+    $(restartBttn).on("click", startGame);
 
 }
 
